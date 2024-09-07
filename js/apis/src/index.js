dbc4029bfe8d57593942c3eab2cae74b1d54e87a
jaylate <178095084+jaylate@users.noreply.github.com>
2024-09-07 17:57:53 +0300

    API fetch: add search field for GIFs and error handling

--- js/apis/src/index.js
+++ js/apis/src/index.js
@@ -1,16 +1,27 @@
 const img = document.querySelector("img");
 const apiKey = process.env.GIPHY_API_KEY;
-function fetchGif() {
-  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=cats`, {
-    mode: "cors",
-  })
+function fetchGif(search) {
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${search}`,
+    {
+      mode: "cors",
+    },
+  )
     .then(function (response) {
-      return response.json();
+      if (response.ok) return response.json();
+      alert("An issue occured, please check your key");
+      return Promise.reject(response);
     })
     .then(function (response) {
       img.src = response.data.images.original.url;
+    })
+    .catch((error) => {
+      console.log(error);
     });
 }
 const button = document.querySelector("button");
-button.addEventListener("click", fetchGif);
+const searchField = document.querySelector("#search");
+button.addEventListener("click", () => {
+  fetchGif(searchField.value);
+});
 fetchGif();
